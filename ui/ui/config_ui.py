6e7a6195abb3192946d2baef66544a5aefef575f
Eran Raichstein <eranra@il.ibm.com>
2024-11-12 09:16:17 +0200

    updated to use input_path and remove base_path

--- ui/ui/config_ui.py
+++ ui/ui/config_ui.py
@@ -24,16 +24,16 @@ class ConfigUI:
         # allows the user to configure the base path for the prediction model
         with st.form(key='config-form'):
             st.subheader("Configure the base path of the prediction model")
-            st.text_input("Base path", key="base_path", value=get_config("job", "base_path"),
-                          help="The base path of the application prediction model")
+            st.text_input("Input path", key="input_path", value=get_config("job", "input_path"),
+                          help="The input path of the prediction model")
             st.form_submit_button("Save")
-            if st.session_state.base_path != config["job"]["base_path"]:
-                if not os.path.isdir(st.session_state.base_path):
+            if st.session_state.input_path != config["job"]["input_path"]:
+                if not os.path.isdir(st.session_state.input_path):
                     st.error('The provided folder does not exist. Please provide a valid folder path.')
                     return
 
-                config["job"]["base_path"] = st.session_state.base_path
-                logger.info(f"Base path changed to {st.session_state.base_path}")
+                config["job"]["input_path"] = st.session_state.input_path
+                logger.info(f"Input path changed to {st.session_state.input_path}")
                 # Persist the configuration
                 save_config()
 
