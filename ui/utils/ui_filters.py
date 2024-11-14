69730377b8a94e44dc6e0c1ae9164868763a8d19
Eran Raichstein <eranra@il.ibm.com>
2024-11-14 11:38:04 +0200

    add exeption handling

--- ui/utils/ui_filters.py
+++ ui/utils/ui_filters.py
@@ -1,3 +1,5 @@
+import logging
+
 import pandas as pd
 import streamlit as st
 from pandas.api.types import (
@@ -6,8 +8,9 @@ from pandas.api.types import (
     is_numeric_dtype,
     is_object_dtype,
 )
-from pandas.core.dtypes.common import is_unsigned_integer_dtype, is_string_dtype, is_integer_dtype
+from pandas.core.dtypes.common import is_string_dtype, is_integer_dtype
 
+logger = logging.getLogger(__name__)
 
 def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
     """
@@ -42,15 +45,29 @@ def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
     with modification_container:
         to_filter_columns = st.multiselect("Filter dataframe on", df.columns)
         for column in to_filter_columns:
-            left, right = st.columns((1, 20))
-            left.write("↳")
-            if is_integer_dtype(df[column]):
-                _min = int(df[column].min())
-                _max = int(df[column].max())
-                step = 1
-                if _min == _max:
-                    pass
-                else:
+            try:
+                left, right = st.columns((1, 20))
+                left.write("↳")
+                if is_integer_dtype(df[column]):
+                    _min = int(df[column].min())
+                    _max = int(df[column].max())
+                    step = 1
+                    if _min == _max:
+                        pass
+                    else:
+                        user_num_input = right.slider(
+                            f"Values for {column}",
+                            _min,
+                            _max,
+                            (_min, _max),
+                            step=step,
+                        )
+                        df = df[df[column].between(*user_num_input)]
+
+                elif is_numeric_dtype(df[column]):
+                    _min = float(df[column].min())
+                    _max = float(df[column].max())
+                    step = (_max - _min) / 100
                     user_num_input = right.slider(
                         f"Values for {column}",
                         _min,
@@ -59,43 +76,31 @@ def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
                         step=step,
                     )
                     df = df[df[column].between(*user_num_input)]
-
-            elif is_numeric_dtype(df[column]):
-                _min = float(df[column].min())
-                _max = float(df[column].max())
-                step = (_max - _min) / 100
-                user_num_input = right.slider(
-                    f"Values for {column}",
-                    _min,
-                    _max,
-                    (_min, _max),
-                    step=step,
-                )
-                df = df[df[column].between(*user_num_input)]
-            elif is_string_dtype(df[column]):
-                user_cat_input = right.multiselect(
-                    f"Values for {column}",
-                    df[column].unique(),
-                    default=list(df[column].unique()),
-                )
-                df = df[df[column].isin(user_cat_input)]
-            elif is_datetime64_any_dtype(df[column]):
-                user_date_input = right.date_input(
-                    f"Values for {column}",
-                    value=(
-                        df[column].min(),
-                        df[column].max(),
-                    ),
-                )
-                if len(user_date_input) == 2:
-                    user_date_input = tuple(map(pd.to_datetime, user_date_input))
-                    start_date, end_date = user_date_input
-                    df = df.loc[df[column].between(start_date, end_date)]
-            else:
-                user_text_input = right.text_input(
-                    f"Substring or regex in {column}",
-                )
-                if user_text_input:
-                    df = df[df[column].str.contains(user_text_input)]
-
+                elif is_string_dtype(df[column]):
+                    user_cat_input = right.multiselect(
+                        f"Values for {column}",
+                        df[column].unique(),
+                        default=list(df[column].unique()),
+                    )
+                    df = df[df[column].isin(user_cat_input)]
+                elif is_datetime64_any_dtype(df[column]):
+                    user_date_input = right.date_input(
+                        f"Values for {column}",
+                        value=(
+                            df[column].min(),
+                            df[column].max(),
+                        ),
+                    )
+                    if len(user_date_input) == 2:
+                        user_date_input = tuple(map(pd.to_datetime, user_date_input))
+                        start_date, end_date = user_date_input
+                        df = df.loc[df[column].between(start_date, end_date)]
+                else:
+                    user_text_input = right.text_input(
+                        f"Substring or regex in {column}",
+                    )
+                    if user_text_input:
+                        df = df[df[column].str.contains(user_text_input)]
+            except Exception as e:
+                logger.info(f"Error filtering column {column}: {e}")
     return df
