3f0a84624157d5f1b2cb2c2ae5c043d50f85ed6f
rachelt44 <rachelt@il.ibm.com>
2025-01-21 20:04:20 +0200

    change number of samples in randomized HPO

--- arise_predictions/utils/constants.py
+++ arise_predictions/utils/constants.py
@@ -44,7 +44,7 @@ AM_OUTPUT_PATH_SUFFIX = "ARISE-auto-models"
 AM_NUM_JOBS = "num_jobs"
 AM_RANKINGS_FILE = "all-star-rankings.csv"
 AM_META_LEARNER_PREFIX = "meta-learner-"
-AM_N_ITER_RANDOM_HPO = 20
+AM_N_ITER_RANDOM_HPO = 50
 
 # auto model column headers
 AM_COL_ESTIMATOR = "estimator"
